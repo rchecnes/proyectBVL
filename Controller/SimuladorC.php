3e551840494ceaee65aaeef52d4b53d8887fc028
checnes <ricardochec@gmail.com>
2017-06-16 10:13:58 -0500

    creando formulas

--- Controller/SimuladorC.php
+++ Controller/SimuladorC.php
@@ -32,18 +32,43 @@ function indexAction(){
 	echo $nom_grupo;
 }*/
 
+function getComision($tipo){
+
+	include('../Config/Conexion.php');
+	$link = getConexion();
+
+	$sql  = "SELECT * FROM comision WHERE concep='$tipo' LIMIT 1";
+	$resp = mysqli_query($link,$sql);
+	$r    = mysqli_fetch_array($resp);
+
+	return array(
+		'COM_SAB'    =>$r['comis_neta_sab'],
+		'BASE_SAB'   =>5000,
+		'MIN_SAB'    =>50,
+		'COM_BVL'    =>$r['retrib_bvl'],
+		'COM_SMV'    =>$r['contrib_smv'],
+		'F_GARANT'   =>$r['fondo_garant'],
+		'VAL_IGV'    =>$r['igv'],
+		'F_LIQUI'    =>$r['fondo_liq'],
+		'MIN_CAVAL'  =>5,
+		'BASE_CAVAL' =>12210.01
+	);
+}
+
 function datoscabAction(){
 	
 	include('../Config/Conexion.php');
 	$link = getConexion();
 
+	$com = getComision('contado');
+
 	$cod_emp         = $_GET['cod_emp'];
 	$tipo            = $_GET['tipo'];
-	$monto_estimado  = ($_GET['monto_estimado']!='' && $_GET['monto_estimado']>0)?$_GET['monto_estimado']:0;
-	$precio_unitario = ($_GET['precio_unitario']!='' && $_GET['precio_unitario']>0)?$_GET['precio_unitario']:0;
+	$monto_estimado  = ($_GET['monto_estimado']!='' && (float)$_GET['monto_estimado']>0)?(float)$_GET['monto_estimado']:0;
+	$precio_unitario = ($_GET['precio_unitario']!='' && (float)$_GET['precio_unitario']>0)?(float)$_GET['precio_unitario']:0;
 
-	$cz_cn_fin = 500.00;
-	$cz_mn_fin = 0;
+	$cz_cn_fin = 5000.00;
+	$cz_ci_fin = 0;
 
 	if ($tipo == 'uno') {
 
@@ -56,20 +81,21 @@ function datoscabAction(){
 	}elseif ($tipo == 'dos') {
 
 		$cz_cn_fin = $monto_estimado;
-		$cz_mn_fin = $precio_unitario;
+		$cz_ci_fin = $precio_unitario;
+		
 	}
 	
 	$cant_acc = ($cz_cn_fin > 0 && $cz_ci_fin>0)?$cz_cn_fin/$cz_ci_fin:0;
 	$mont_neg = $cz_ci_fin*$cant_acc;
-	
+
 	$info = array(
 				//CABECERA
-				'mont_est'=>number_format($cz_cn_fin,3,'.',','),
-				'cant_acc'=>number_format($cant_acc,3,'.',','),
-				'pre_unit'=>number_format($cz_ci_fin,3,'.',','),
+				'mont_est'=>number_format($cz_cn_fin,3,'.',''),
+				'pre_unit'=>number_format($cz_ci_fin,3,'.',''),
+				'cant_acc'=>number_format($cant_acc,0,'.',','),				
 				'mont_neg'=>number_format($mont_neg,3,'.',','),
 				//COMPRA
-				'c_comision_sab' =>number_format(0,2,'.',','),
+				'c_comision_sab' =>($mont_neg>$com['BASE_SAB'])?number_format($mont_neg*($com['COM_SAB']/100),2,'.',''):number_format($com['MIN_SAB'],2,'.',''),
 				'c_cuota_bvl'    =>number_format(0,2,'.',','),
 				'c_f_garantia'   =>number_format(0,2,'.',','),
 				'c_cavali'       =>number_format(0,2,'.',','),
