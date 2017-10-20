4727bc2b3ee3d665e146ea9960940207efc11c5e
rchecnes <rchecnes@acuario.com.pe>
2017-10-20 18:49:59 -0500

    casi fin de la extraccion

--- Controller/PruebaC.php
+++ Controller/PruebaC.php
@@ -1,104 +1,194 @@
 <?php
-//$url = "https://www.bvl.com.pe/informacion-general-empresa/-/informacion/VOLCABC1/64801/4";
-$url = "https://www.bvl.com.pe/web/guest/informacion-general-empresa?p_p_id=informaciongeneral_WAR_servicesbvlportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_count=1&_informaciongeneral_WAR_servicesbvlportlet_cmd=getListaHistoricoCotizaciones&_informaciongeneral_WAR_servicesbvlportlet_codigoempresa=60800&_informaciongeneral_WAR_servicesbvlportlet_nemonico=ATACOBC1&_informaciongeneral_WAR_servicesbvlportlet_tabindex=4&_informaciongeneral_WAR_servicesbvlportlet_jspPage=%2Fhtml%2Finformaciongeneral%2Fview.jsp";
+error_reporting(E_ALL);
+//require_once('simple_html_dom.php');
+//V2. AGREGADO
+function normalize_str($str)
+{
+$replace = array (
+    '&lt;' => '', '&gt;' => '', '&#039;' => '', '&amp;' => '',
+    '&quot;' => '', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'Ae',
+    '&Auml;' => 'A', 'Å' => 'A', 'Ā' => 'A', 'Ą' => 'A', 'Ă' => 'A', 'Æ' => 'Ae',
+    'Ç' => 'C', 'Ć' => 'C', 'Č' => 'C', 'Ĉ' => 'C', 'Ċ' => 'C', 'Ď' => 'D', 'Đ' => 'D',
+    'Ð' => 'D', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ē' => 'E',
+    'Ę' => 'E', 'Ě' => 'E', 'Ĕ' => 'E', 'Ė' => 'E', 'Ĝ' => 'G', 'Ğ' => 'G',
+    'Ġ' => 'G', 'Ģ' => 'G', 'Ĥ' => 'H', 'Ħ' => 'H', 'Ì' => 'I', 'Í' => 'I',
+    'Î' => 'I', 'Ï' => 'I', 'Ī' => 'I', 'Ĩ' => 'I', 'Ĭ' => 'I', 'Į' => 'I',
+    'İ' => 'I', 'Ĳ' => 'IJ', 'Ĵ' => 'J', 'Ķ' => 'K', 'Ł' => 'K', 'Ľ' => 'K',
+    'Ĺ' => 'K', 'Ļ' => 'K', 'Ŀ' => 'K', 'Ñ' => 'N', 'Ń' => 'N', 'Ň' => 'N',
+    'Ņ' => 'N', 'Ŋ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O',
+    'Ö' => 'Oe', '&Ouml;' => 'Oe', 'Ø' => 'O', 'Ō' => 'O', 'Ő' => 'O', 'Ŏ' => 'O',
+    'Œ' => 'OE', 'Ŕ' => 'R', 'Ř' => 'R', 'Ŗ' => 'R', 'Ś' => 'S', 'Š' => 'S',
+    'Ş' => 'S', 'Ŝ' => 'S', 'Ș' => 'S', 'Ť' => 'T', 'Ţ' => 'T', 'Ŧ' => 'T',
+    'Ț' => 'T', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'Ue', 'Ū' => 'U',
+    '&Uuml;' => 'Ue', 'Ů' => 'U', 'Ű' => 'U', 'Ŭ' => 'U', 'Ũ' => 'U', 'Ų' => 'U',
+    'Ŵ' => 'W', 'Ý' => 'Y', 'Ŷ' => 'Y', 'Ÿ' => 'Y', 'Ź' => 'Z', 'Ž' => 'Z',
+    'Ż' => 'Z', 'Þ' => 'T', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a',
+    'ä' => 'ae', '&auml;' => 'ae', 'å' => 'a', 'ā' => 'a', 'ą' => 'a', 'ă' => 'a',
+    'æ' => 'ae', 'ç' => 'c', 'ć' => 'c', 'č' => 'c', 'ĉ' => 'c', 'ċ' => 'c',
+    'ď' => 'd', 'đ' => 'd', 'ð' => 'd', 'è' => 'e', 'é' => 'e', 'ê' => 'e',
+    'ë' => 'e', 'ē' => 'e', 'ę' => 'e', 'ě' => 'e', 'ĕ' => 'e', 'ė' => 'e',
+    'ƒ' => 'f', 'ĝ' => 'g', 'ğ' => 'g', 'ġ' => 'g', 'ģ' => 'g', 'ĥ' => 'h',
+    'ħ' => 'h', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ī' => 'i',
+    'ĩ' => 'i', 'ĭ' => 'i', 'į' => 'i', 'ı' => 'i', 'ĳ' => 'ij', 'ĵ' => 'j',
+    'ķ' => 'k', 'ĸ' => 'k', 'ł' => 'l', 'ľ' => 'l', 'ĺ' => 'l', 'ļ' => 'l',
+    'ŀ' => 'l', 'ñ' => 'n', 'ń' => 'n', 'ň' => 'n', 'ņ' => 'n', 'ŉ' => 'n',
+    'ŋ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'oe',
+    '&ouml;' => 'oe', 'ø' => 'o', 'ō' => 'o', 'ő' => 'o', 'ŏ' => 'o', 'œ' => 'oe',
+    'ŕ' => 'r', 'ř' => 'r', 'ŗ' => 'r', 'š' => 's', 'ù' => 'u', 'ú' => 'u',
+    'û' => 'u', 'ü' => 'ue', 'ū' => 'u', '&uuml;' => 'ue', 'ů' => 'u', 'ű' => 'u',
+    'ŭ' => 'u', 'ũ' => 'u', 'ų' => 'u', 'ŵ' => 'w', 'ý' => 'y', 'ÿ' => 'y',
+    'ŷ' => 'y', 'ž' => 'z', 'ż' => 'z', 'ź' => 'z', 'þ' => 't', 'ß' => 'ss',
+    'ſ' => 'ss', 'ый' => 'iy', 'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G',
+    'Д' => 'D', 'Е' => 'E', 'Ё' => 'YO', 'Ж' => 'ZH', 'З' => 'Z', 'И' => 'I',
+    'Й' => 'Y', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+    'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F',
+    'Х' => 'H', 'Ц' => 'C', 'Ч' => 'CH', 'Ш' => 'SH', 'Щ' => 'SCH', 'Ъ' => '',
+    'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'YU', 'Я' => 'YA', 'а' => 'a',
+    'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo',
+    'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l',
+    'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's',
+    'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+    'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e',
+    'ю' => 'yu', 'я' => 'ya','&eacute;' => 'e','<br>'=>'','<br/>'=>''
+);
+	//'(' => '',')' => '',
 
-# url_get_contents function by Andy Langton: https://andylangton.co.uk/
-function getHeaders(){
+$str = str_replace(array_keys($replace), array_values($replace), $str);
 
-	$curlHeaders = array (
-                    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding: gzip, deflate',
-                    'Accept-Language: en-US,en;q=0.5',
-                    'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0.1',
-                    'Connection: Keep-Alive',
-                    'Pragma: no-cache',
-                    'Referer: http://example.com/',
-                    'Host: hostname',
-                    'Cache-Control: no-cache',
-                    'Cookie: visid_incap_185989=9v1q8Ar0ToSOja48BRmb8nn1GFUAAAAAQUIPAAAAAABCRWagbDIfmlN9NTrcvrct; incap_ses_108_185989=Z1orY6Bd0z3nGYE2lbJ/AXn1GFUAAAAAmb41m+jMLFCJB1rTIF28Mg==; _ga=GA1.3.637468927.1427699070; _gat=1; frontend=rqg7g9hp2ht788l309m7gk8qi7; _gat_UA-1279175-12=1; __utma=233911437.637468927.1427699070.1427699078.1427699078.1; __utmb=233911437.2.10.1427699078; __utmc=233911437; __utmz=233911437.1427699078.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt_UA-1279175-1=1; _cb_ls=1; _chartbeat2=S0WVXDwMWnCFBgQp.1427699081322.1427699232786.1; PRUM_EPISODES=s=1427699568560&r=http%3A//example.com/');
-    return $curlHeaders;
+return $str;
 }
 
-function url_get_contents($url,$useragent='cURL',$headers=false,$follow_redirects=false,$debug=false) {
-	 
-	# initialise the CURL library
-	$ch = curl_init();
-	 
-	# specify the URL to be retrieved
-	curl_setopt($ch, CURLOPT_URL,$url);
-	 
-	# we want to get the contents of the URL and store it in a variable
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-	 
-	# specify the useragent: this is a required courtesy to site owners
-	curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-	
-	#Set headers
-	//curl_setopt ($ch, CURLOPT_HTTPHEADER, getHeaders());
 
-	# ignore SSL errors
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	 
-	# return headers as requested
-	if ($headers==true){
-	curl_setopt($ch, CURLOPT_HEADER,1);
-	}
-	 
-	# only return headers
-	if ($headers=='headers only') {
-	curl_setopt($ch, CURLOPT_NOBODY ,1);
-	}
-	 
-	# follow redirects - note this is disabled by default in most PHP installs from 4.4.4 up
-	if ($follow_redirects==true) {
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
-	}
-	 
-	# if debugging, return an array with CURL's debug info and the URL contents
-	if ($debug==true) {
-	$result['contents']=curl_exec($ch);
-	$result['info']=curl_getinfo($ch);
-	}
-	 
-	# otherwise just return the contents as a variable
-	else $result=curl_exec($ch);
-	 
-	# free resources
-	curl_close($ch);
-	 
-	# send back the data
-	return $result;
+ $tmpFile = uniqid();
+ mb_internal_encoding("UTF-8");
+
+ //$database = $_SESSION['xcom_comp'];
+//Copyright (C) 2010  Jonathan Preece
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+//REMOVER CARACETERES ESPECIALES
+function clean($string) {
+   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+
+   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+   return strtoupper($string);
 }
-//echo url_get_contents($url);
 
-function wget($url, $follow = true) {
-
-    $host = parse_url($url);
-
-    $agent       = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0.1';
-    $curlHeaders = array(
-        'Accept: application/json, text/javascript, */*',
-        'Accept-Encoding: gzip, deflate, br',
-        'Accept-Language: es-ES,es;q=0.8',
-        'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
-        'Connection: keep-alive',
-        'Pragma: no-cache',
-        'Referer: https://www.bvl.com.pe/informacion-general-empresa/-/informacion/ATACOBC1/60800/4',
-        'Host: www.bvl.com.pe', // building host header
-        'Cache-Control: max-age=0',
-        'Cookie: JSESSIONID=7F030E65963427DE15B2823979E2158E; COOKIE_SUPPORT=true; GUEST_LANGUAGE_ID=es_ES; _ga=GA1.3.1899256373.1508162172; _gid=GA1.3.2040075662.1508162172; LFR_SESSION_STATE_20159=1508165823695'
+function cleanTxImg($string) {
+   $replace = array (
+    ' ' => '', '-' => '' 
     );
-    $ch          = curl_init();
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $curlHeaders);
-    curl_setopt($ch, CURLOPT_HEADER, TRUE);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $follow); // following redirects or not
-    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    $result      = curl_exec($ch);
-    return $result;
+
+   $string = str_replace(array_keys($replace), array_values($replace), $string);
+
+   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+   return strtoupper($string);
 }
 
-echo(wget('https://www.bvl.com.pe'));
+function strrpos_count($haystack, $needle, $count)
+{
+    if($count <= 0)
+        return false;
+
+    $len = strlen($haystack);
+    $pos = $len;
+
+    for($i = 0; $i < $count && $pos; $i++)
+        $pos = strrpos($haystack, $needle, $pos - $len - 1);
+
+    return $pos;
+}
+
+$directorio = "Temp/";
+
+function get_remote_data($url, $post_paramtrs = false) {
+    global $directorio;
+    if(!is_dir("$directorio")) 
+        mkdir("$directorio", 0777);
+    
+    //$tmpFile_=$directorio.$tmpFile;
+    //$tmpFileText=$directorio.$tmpFile.".txt";
+    //$image_file = $directorio."captcha3.jpg";
+    $cookie=$directorio."cookie.txt";
+
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $url);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    if ($post_paramtrs) {
+        curl_setopt($c, CURLOPT_POST, TRUE);
+        curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&" . $post_paramtrs);
+    } curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0");
+    curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;');
+    curl_setopt($c, CURLOPT_MAXREDIRS, 10);
+    $follow_allowed = ( ini_get('open_basedir') || ini_get('safe_mode')) ? false : true;
+    if ($follow_allowed) {
+        curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
+    }curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
+    curl_setopt($c, CURLOPT_REFERER, $url);
+    curl_setopt($c, CURLOPT_TIMEOUT, 60);
+    curl_setopt($c, CURLOPT_AUTOREFERER, true);
+    curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');
+    curl_setopt($c, CURLOPT_COOKIEFILE, $cookie);
+    curl_setopt($c, CURLOPT_COOKIEJAR, $cookie);
+    $data = curl_exec($c);
+    $status = curl_getinfo($c);
+    curl_close($c);
+    preg_match('/(http(|s)):\/\/(.*?)\/(.*\/|)/si', $status['url'], $link);
+    $data = preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/|\/)).*?)(\'|\")/si', '$1=$2' . $link[0] . '$3$4$5', $data);
+    $data = preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/)).*?)(\'|\")/si', '$1=$2' . $link[1] . '://' . $link[3] . '$3$4$5', $data);
+    if ($status['http_code'] == 200) {
+        return $data;
+    } elseif ($status['http_code'] == 301 || $status['http_code'] == 302) {
+        if (!$follow_allowed) {
+            if (empty($redirURL)) {
+                if (!empty($status['redirect_url'])) {
+                    $redirURL = $status['redirect_url'];
+                }
+            } if (empty($redirURL)) {
+                preg_match('/(Location:|URI:)(.*?)(\r|\n)/si', $data, $m);
+                if (!empty($m[2])) {
+                    $redirURL = $m[2];
+                }
+            } if (empty($redirURL)) {
+                preg_match('/href\=\"(.*?)\"(.*?)here\<\/a\>/si', $data, $m);
+                if (!empty($m[1])) {
+                    $redirURL = $m[1];
+                }
+            } if (!empty($redirURL)) {
+                $t = debug_backtrace();
+                return call_user_func($t[0]["function"], trim($redirURL), $post_paramtrs);
+            }
+        }
+    } return "ERRORCODE22 with $url!!<br/>Last status codes<b/>:" . json_encode($status) . "<br/><br/>Last data got<br/>:$data";
+}
+
+
+
+$txtSunat=$directorio."richard.txt";
+
+$xxx=get_remote_data("https://www.bvl.com.pe/web/guest/informacion-general-empresa?p_p_id=informaciongeneral_WAR_servicesbvlportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_count=1&_informaciongeneral_WAR_servicesbvlportlet_cmd=getListaHistoricoCotizaciones&_informaciongeneral_WAR_servicesbvlportlet_codigoempresa=60800&_informaciongeneral_WAR_servicesbvlportlet_nemonico=ATACOBC1&_informaciongeneral_WAR_servicesbvlportlet_tabindex=4&_informaciongeneral_WAR_servicesbvlportlet_jspPage=%2Fhtml%2Finformaciongeneral%2Fview.jsp","_informaciongeneral_WAR_servicesbvlportlet_anoini=2017&_informaciongeneral_WAR_servicesbvlportlet_mesini=10&_informaciongeneral_WAR_servicesbvlportlet_anofin=2017&_informaciongeneral_WAR_servicesbvlportlet_mesfin=10&_informaciongeneral_WAR_servicesbvlportlet_nemonicoselect=ATACOBC1");
+
+//GUARDAR CONTENIDO EN UN TEXTO
+file_put_contents($txtSunat, $xxx);
+
+$lines = file($txtSunat);
+
+print_r($lines);
 
 ?>
