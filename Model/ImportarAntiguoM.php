fb93984c2220228a8e4a59ea26032857b9d2defb
rchecnes <rchecnes@acuario.com.pe>
2017-12-26 14:55:23 -0500

    add cambios de formato miles

--- Model/ImportarAntiguoM.php
+++ Model/ImportarAntiguoM.php
@@ -111,7 +111,7 @@ function getPrepareDataAntiguo($empresa, $html){
 	                    'max'=> (double)str_replace(" ","",$e->find('td',3)->plaintext),
 	                    'min'=> (double)str_replace(" ","",$e->find('td',4)->plaintext),
 	                    'prd'=> (double)str_replace(" ","",$e->find('td',5)->plaintext),
-	                    'cn' => (double)str_replace(" ","",$e->find('td',6)->plaintext),
+	                    'cn' => (double)str_replace(",","",$e->find('td',6)->plaintext),
 	                    'mn' => (double)str_replace(",","",$e->find('td',7)->plaintext),
 	                    'fa' => ($fecha_anterior!='')?$fecha_anterior:"0000-00-00",
 	                    'ca' => (double)str_replace(" ","",$e->find('td',9)->plaintext)
