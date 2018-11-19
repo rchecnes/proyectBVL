11d0130e8f6dc1444af5c253198c9f7e9ea9b652
checnes <ricardochec@gmail.com>
2018-11-19 18:15:05 -0500

    add file

--- Controller/CronCotizaEmpresaManC.php
+++ Controller/CronCotizaEmpresaManC.php
@@ -6,33 +6,36 @@ require_once($ruta."/Model/CotizaGrupoM.php");
 require_once($ruta."/Model/ImportarAntiguoM.php");
 
 function getCotizacionGrupoAntiguo(){
+    //http://analisisdevalor.com/Controller/CronCotizaEmpresaManC.php?fec_inicio=20180922&fec_fin=20181115&abc_nemonico=A
 
-    //global $ruta;
     $link       = getConexion();
     $fec_inicio = date('Ymd');
     $fec_fin    = date('Ymd');
+    $abc_nemonico = ($_GET['abc_nemonico']!='')?" AND em.nemonico LIKE '".$_GET['abc_nemonico']."%'":"";
+    $fec_inicio   = $_GET['fec_inicio'];
+    $fec_fin      = $_GET['fec_fin'];
+    if($fec_inicio=='' || $fec_fin ==''){
+        echo "Debe pasar como parametro la fecha de inicio y fin";
+    }
 
     $date      = date('Y-m-d');
     $sqlemp    = "SELECT em.nemonico FROM empresa em
                   LEFT JOIN sector se ON(em.cod_sector=se.cod_sector)
                   WHERE se.estado='1'
                   AND em.estado='1'
-                  AND em.nemonico IN(SELECT s_cd.cd_cod_emp FROM cotizacion_del_dia s_cd WHERE s_cd.cd_cod='$fec_inicio' AND s_cd.cd_ng_nop > 0)";
-                
+                  $abc_nemonico";
+                          ;
     $respemp   = mysqli_query($link, $sqlemp);
 
     $c = 0;
     while ($e = mysqli_fetch_array($respemp)) {
         
         $nemonico = $e['nemonico'];
-
         $url = "http://www.bvl.com.pe/jsp/cotizacion.jsp?fec_inicio=$fec_inicio&fec_fin=$fec_fin&nemonico=$nemonico";
         //$html = file_get_html($url);
         $html = file_get_contents_curl($url);
 
         $new_data = getPrepareDataAntiguo($nemonico, $html);
-
-        //$new_data = ordenarArray($new_data,'f','ASC');
 
         if (count($new_data)>0) {
 
