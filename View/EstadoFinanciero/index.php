1694b697e91ea4814e1a190e98e4e8af51e1b172
checnes <ricardochec@gmail.com>
2019-12-03 10:16:42 -0700

    avance importacion de estados resultados

--- View/EstadoFinanciero/index.php
+++ View/EstadoFinanciero/index.php
@@ -10,7 +10,7 @@
 	<script type="text/javascript">
 		$(document).ready(function(){
 
-			importarInformacion = function(){
+			importarInformacionEstadoFinanciero = function(){
 
 		        $("#loading").show();
 
@@ -26,12 +26,12 @@
 		            data:{cef_nemonico:cef_nemonico,cef_tipo:cef_tipo,cef_anio:cef_anio,cef_trim:cef_trim,cef_peri:cef_peri},
 		            success:function(data){
 		                $("#loading").hide();
-						buscarImportado();
+						buscarImportadoEstadoFinanciero();
 		            }
 		        });
 		    }
 
-			buscarImportado = function(){
+			buscarImportadoEstadoFinanciero = function(){
 
 				$("#loading").show();
 
@@ -53,17 +53,61 @@
 				});
 			}
 
-			buscarImportado();
+			buscarImportadoEstadoFinanciero();
+
+			importarInformacionEstadoResultado = function(){
+
+				$("#loading").show();
+
+				var cef_nemonico = $("#cef_nemonico").val();
+				var cef_tipo = $("#cef_tipo").val();
+				var cef_anio = $("#cef_anio").val();
+				var cef_trim = $("#cef_trim").val();
+				var cef_peri = $("#cef_peri").val();
+
+				$.ajax({
+					type:'GET',
+					url: '../Controller/EstadoFinancieroC.php?accion=importarmanual',
+					data:{cef_nemonico:cef_nemonico,cef_tipo:cef_tipo,cef_anio:cef_anio,cef_trim:cef_trim,cef_peri:cef_peri},
+					success:function(data){
+						$("#loading").hide();
+						buscarImportadoEstadoResultado();
+					}
+				});
+			}
+
+			buscarImportadoEstadoResultado = function(){
+
+				$("#loading").show();
+
+				var cef_nemonico = $("#cef_nemonico").val();
+				var cef_tipo = $("#cef_tipo").val();
+				var cef_anio = $("#cef_anio").val();
+				var cef_trim = $("#cef_trim").val();
+				var cef_peri = $("#cef_peri").val();
+
+				$.ajax({
+					type:'GET',
+					url: '../Controller/EstadoFinancieroC.php?accion=listar',
+					data:{cef_nemonico:cef_nemonico,cef_tipo:cef_tipo,cef_anio:cef_anio,cef_trim:cef_trim,cef_peri:cef_peri},
+					success:function(data){
+
+						$("#divHistorico").html(data);
+						$("#loading").hide();
+					}
+				});
+			}
 		});
 
 	</script>
 	<div class="container">
 		<div class="tabbable">
 	        <ul class="nav nav-tabs" id="tabs">
-	          <li class="active"><a data-toggle="tab" href="#cierre_del_dia"><h4>Estado Financiero</h4></a></li>
+	          <li class="active"><a data-toggle="tab" href="#tab_estado_financiero"><h4>Estado Financiero</h4></a></li>
+			  <li><a data-toggle="tab" href="#tab_estado_resultado"><h4>Estado De Resultado</h4></a></li>
 	        </ul>
 	        <div class="tab-content">
-	            <div id="cierre_del_dia" class="tab-pane fade in active">
+	            <div id="tab_estado_financiero" class="tab-pane fade in active">
 					<div class="row">
 						<div class="col-lg-3">
 							<div class="form-group">
@@ -110,7 +154,7 @@
 								<label>Periodo:</label>
 								<select id="cef_peri" name="cef_peri" class="form-control">
 									<option value="T" selected="selected">Trimestral No Auditada</option>
-									<option value="A">Auditada Anual</option>
+									<option value="A" disabled>Auditada Anual</option>
 								</select>
 							</div>
 						</div>
@@ -129,8 +173,81 @@
 						<div class="col-lg-6">
 							<label>&nbsp;</label>
 							<div class="form-group">
-								<input name="button" type="button" class="btn btn-default" id="button" value="Buscar Importado" onclick="buscarImportado()">
-								<input name="button" type="button" class="btn btn-success" id="button" value="Importar Informacion" onclick="importarInformacion()">
+								<input name="button" type="button" class="btn btn-default" id="button" value="Buscar Importado" onclick="buscarImportadoEstadoFinanciero()">
+								<input name="button" type="button" class="btn btn-success" id="button" value="Importar Informacion" onclick="importarInformacionEstadoFinanciero()">
+								<img src="../Assets/img/load.gif" id="loading" style="display: none">
+							</div>
+						</div>
+					</div>
+	            </div>
+				<div id="tab_estado_resultado" class="tab-pane fade in">
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label>Empresa:</label>
+								<?php   
+									$params = array(
+										'select' => array('id'=>'cef_nemonico', 'name'=>'cef_nemonico', 'class'=>'form-control'),
+										'sql'    => "SELECT nemonico,nombre,moneda FROM empresa WHERE estado=1 AND imp_sit_fin!=''",
+										'attrib' => array('value'=>'nemonico','desc'=>'nemonico,nombre,moneda', 'concat'=>' - ','descextra'=>''),
+										'empty'  => false,
+										'defect' => 'GRAMONC1',
+										'edit'   => '',
+										'enable' => 'enable'
+									);
+
+									Combobox($link, $params);
+								?>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label>Tipo:</label>
+								<select id="cef_tipo" name="cef_tipo" class="form-control">
+									<option value="I">Individual</option>
+									<option value="C" selected="selected">Consolidada</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label>Año:</label>
+								<select id="cef_anio" name="cef_anio" class="form-control">
+								<?php
+								for($a=2000;$a<=2050;$a++){
+									$selected = ($a == date('Y'))?'selected="selected"':'';
+									echo '<option value="'.$a.'" '.$selected.'>'.$a.'</option>';
+								}
+								?>
+								</select>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label>Periodo:</label>
+								<select id="cef_peri" name="cef_peri" class="form-control">
+									<option value="T" selected="selected">Trimestral No Auditada</option>
+									<option value="A" disabled>Auditada Anual</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label>Trimestre:</label>
+								<select id="cef_trim" name="cef_trim" class="form-control">
+									<option value="1">Primer Trimestre</option>
+									<option value="2">Segundo Trimestre</option>
+									<option value="3" selected="selected">Tercer Trimestre</option>
+								</select>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-6">
+							<label>&nbsp;</label>
+							<div class="form-group">
+								<input name="button" type="button" class="btn btn-default" id="button" value="Buscar Importado" onclick="buscarImportadoEstadoResultado()">
+								<input name="button" type="button" class="btn btn-success" id="button" value="Importar Informacion" onclick="importarInformacionEstadoResultado()">
 								<img src="../Assets/img/load.gif" id="loading" style="display: none">
 							</div>
 						</div>
---
+++ View/EstadoResultado/listar.php
@@ -0,0 +1,41 @@
+<p>Cantidad Registros:<b><?=$nro_reg?></b></p>
+<style>
+.cab{
+    background-color: #a7a9ac;
+    color:#FFF;
+}
+</style>
+<table class="table table-bordered">
+    <tr>
+        <th colspan="5" style="background:#b11b21;color:#FFF;text-align: center;"><?=$nombre_empresa?></th>
+    </tr>
+    <tr>
+        <th colspan="5" style="background:#434343;color:#FFF;text-align: center;">ESTADO DE SITUACION FINANCIERA / STATEMENT OF FINANCIAL POSITION</th>
+    </tr>
+    
+    <?php
+    while ($ub = mysqli_fetch_array($res)) {
+
+        $cef_cod  = $ub['cef_cod'];
+        $cef_cod_bvl  = $ub['cef_cod_bvl'];
+        $cef_nomb  = $ub['cef_nomb'];
+        $def_nemonico  = $ub['def_nemonico'];
+        $def_val_de  = $ub['def_val_de'];
+        $def_val_ha  = $ub['def_val_ha'];
+        $cef_cab_det  = $ub['cef_cab_det'];
+
+        echo "<tr>";
+        if($cef_cab_det == 'CAB'){
+            $def_val_de_new = ($def_val_de!=0)?number_format($def_val_de,0,'',','):'';
+            $def_val_ha_new = ($def_val_ha!=0)?number_format($def_val_ha,0,'',','):'';
+            echo "<th class='cab'>$cef_cod_bvl</th><th colspan='2' class='cab'>$cef_nomb</th><th class='cab' align='right'>$def_val_de_new</th><th class='cab' align='right'>$def_val_ha_new</th>";
+        }else{
+            $def_val_de_new = ($def_val_de!='')?number_format($def_val_de,0,'',','):'';
+            $def_val_ha_new = ($def_val_ha!='')?number_format($def_val_ha,0,'',','):'';
+            echo "<td>$cef_cod_bvl</td><td>&nbsp;</td><td>$cef_nomb</td><td align='right'>$def_val_de_new</td><td align='right'>$def_val_ha_new</td>";
+        }
+        echo "</tr>";
+    
+    }
+    ?>
+</table>
